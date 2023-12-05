cd986ec1975eb19dc353667eb56d480d3ce4bfe4
Guillermo Mira Osuna <miraosuna@usal.es>
2023-12-05 11:34:19 +0100

    sufriendo

--- srcs/2-frontend-WP/scriptfront.sh
+++ srcs/2-frontend-WP/scriptfront.sh
@@ -4,7 +4,7 @@ cd /var/www/html && rm -rf * #the remove is in case volumes left something store
 wp core download --allow-root
 # change lines in wp-config.php file to connect with database
 wp config create --dbname=$DB_NAME --dbuser=$MYSQL_ADMIN_USER --dbpass=$MYSQL_ROOT_PASSWORD2 --dbhost=container-mariadb --allow-root
-wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root
 # creates a new user account.
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 # installs the Astra theme and activates it for the site. The --activate flag tells WP-CLI to make the theme the active theme for the site.
