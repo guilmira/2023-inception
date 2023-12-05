1f490afbd42cc1d56b65cb35d31923358cd667e2
Guillermo Mira Osuna <miraosuna@usal.es>
2023-12-05 12:49:48 +0100

    2

--- srcs/2-frontend-WP/scriptfront.sh
+++ srcs/2-frontend-WP/scriptfront.sh
@@ -8,7 +8,7 @@ wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR 
 # creates a new user account.
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 # installs the Astra theme and activates it for the site. The --activate flag tells WP-CLI to make the theme the active theme for the site.
-	#wp theme install astra --activate --allow-root
+wp theme install astra --activate --allow-root
 mkdir /run/php #-->this is extremely necessary
 # starts the PHP-FPM service in the foreground. The -F flag tells PHP-FPM to run in the foreground, rather than as a daemon in the background.
 /usr/sbin/php-fpm7.3 -F
