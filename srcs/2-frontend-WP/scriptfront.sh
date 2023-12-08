74321f59d882ad6feca35e1bab82a0e4568c9be3
Guillermo Mira Osuna <miraosuna@usal.es>
2023-12-08 11:19:06 +0100

    standard commit

--- srcs/2-frontend-WP/scriptfront.sh
+++ srcs/2-frontend-WP/scriptfront.sh
@@ -6,7 +6,7 @@ wp core download --allow-root
 wp config create --dbname=$DB_NAME --dbuser=$MYSQL_ADMIN_USER --dbpass=$MYSQL_ROOT_PASSWORD2 --dbhost=container-mariadb --allow-root
 wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root
 # creates a new user account.
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+#wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 # installs the Astra theme and activates it for the site. The --activate flag tells WP-CLI to make the theme the active theme for the site.
 wp theme install astra --activate --allow-root
 mkdir /run/php #-->extremely necessary
